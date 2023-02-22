b080425d386b377bcf8922b18799dd960cb97622
Paritosh Kadam <priyatamkadam@gmail.com>
2023-02-22 19:12:24 +0530

    no idea

--- controllers/ticketControllers.js
+++ controllers/ticketControllers.js
@@ -4,9 +4,7 @@ const User = require("../modals/userModal")
 
 const buyTicket = expressAsyncHandler(async (req, res) => {
     
-    
-    
-    let verif;
+    let verif = true;
     let { email, ticketname} = req.body;
     if (verif) {
 
@@ -26,6 +24,24 @@ const buyTicket = expressAsyncHandler(async (req, res) => {
         res.status(400)
     }
 });
+
+const getUserTickets = async (req, res) => {
+    let data = await User.findOne({ email: req.body.email })
+    let arr = data.tickets
+    let ans =[]
+    try {
+        for (let i = 0; i < arr.length; i++){
+            let tic = await Ticket.findById(arr[i]._id);
+            ans.push(tic);
+        }
+        res.status(201).send(ans)        
+    }
+    catch (err) {
+        console.log(err)
+        res.send(400).send("error h")
+    }
+
+}
 
 const getAllTickets = async (req, res) => {
     let data = await Ticket.find({})
@@ -73,4 +89,4 @@ const ticketBool = expressAsyncHandler(async (req, res) => {
     }
 })
 
-module.exports = {buyTicket, ticketBool, addTicket, getAllTickets}
+module.exports = {buyTicket, ticketBool, addTicket, getAllTickets, getUserTickets}
--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {buyTicket, addTicket, ticketBool, getAllTickets} = require("../controllers/ticketControllers")
+const {buyTicket, addTicket, ticketBool, getAllTickets, getUserTickets} = require("../controllers/ticketControllers")
 
 router.route("/buy").post(buyTicket)
 router.route("/attendence/").post(ticketBool)
 router.route("/addTicket").post(addTicket)
 router.route("/getAllTs").get(getAllTickets)
+router.route("/getUserTickets").post(getUserTickets)
 
 module.exports = router;
