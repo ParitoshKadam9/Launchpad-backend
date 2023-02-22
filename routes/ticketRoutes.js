59a2fde8210b9d40f79a505c5dd18f4d0e6d15a1
Ayush Bhauwala <ayushbhauwala07@gmail.com>
2023-02-22 17:03:32 +0530

    Added route to get orderid and updated buy route
    Added route to get orderid for frontend for razorpay. Updated buyTicket to verify payment and added params to buy route. Sending "success" on successful payment. Updating "User" collection with id instead of email.

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {buyTicket, addTicket, ticketBool, getAllTickets} = require("../controllers/ticketControllers")
+const {
+  buyTicket,
+  addTicket,
+  ticketBool,
+  getAllTickets,
+  Orderid,
+} = require("../controllers/ticketControllers");
 
-router.route("/buy").post(buyTicket)
-router.route("/attendence/").post(ticketBool)
-router.route("/addTicket").post(addTicket)
-router.route("/getAllTs").get(getAllTickets)
+router.route("/buy/:ticketId/:userId").post(buyTicket);
+router.route("/attendence/").post(ticketBool);
+router.route("/addTicket").post(addTicket);
+router.route("/getAllTs").get(getAllTickets);
+router.route("/getOrderId").get(Orderid);
 
 module.exports = router;
