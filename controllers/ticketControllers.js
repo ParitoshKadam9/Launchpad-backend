0ab8c8c0eb8b5229b8e19ae7dfd469b55f0c9f89
Ayush Bhauwala <ayushbhauwala07@gmail.com>
2023-02-22 19:52:36 +0530

    Getting ticket info from backend
    Ticket info comes from backend and fixed bug of userid

--- controllers/ticketControllers.js
+++ controllers/ticketControllers.js
@@ -7,17 +7,20 @@ var crypto = require("crypto");
 //creating orderid to be sent to the frontend.
 var orderid;
 const Orderid = expressAsyncHandler(async (req, res) => {
+  const ticketid = req.params.ticketId;
+  let ticket = await Ticket.findOne({ id: ticketid });
   const instance = new Razorpay({
     key_id: "rzp_test_Ef3Hus21Zu9tyy",
     key_secret: "SgkEyuw6RLMfJjS3ms2Y0TgK",
   });
   const response = await instance.orders.create({
-    amount: 5 * 100,
+    amount: ticket.amount * 100,
     currency: "INR",
   });
   orderid = response.id;
   return res.json({
     id: response.id,
+    amount: ticket.amount * 100,
   });
 });
 
@@ -38,7 +41,7 @@ const buyTicket = expressAsyncHandler(async (req, res) => {
     let ticket = await Ticket.findOne({ id: ticketname });
     console.log(ticket);
     await User.updateOne(
-      { id: userId },
+      { _id: userId },
       { $push: { tickets: { $each: [ticket] } } }
     );
 
@@ -53,22 +56,20 @@ const buyTicket = expressAsyncHandler(async (req, res) => {
 });
 
 const getUserTickets = async (req, res) => {
-    let data = await User.findOne({ email: req.body.email })
-    let arr = data.tickets
-    let ans =[]
-    try {
-        for (let i = 0; i < arr.length; i++){
-            let tic = await Ticket.findById(arr[i]._id);
-            ans.push(tic);
-        }
-        res.status(201).send(ans)        
+  let data = await User.findOne({ email: req.body.email });
+  let arr = data.tickets;
+  let ans = [];
+  try {
+    for (let i = 0; i < arr.length; i++) {
+      let tic = await Ticket.findById(arr[i]._id);
+      ans.push(tic);
     }
-    catch (err) {
-        console.log(err)
-        res.send(400).send("error h")
-    }
-
-}
+    res.status(201).send(ans);
+  } catch (err) {
+    console.log(err);
+    res.send(400).send("error h");
+  }
+};
 
 const getAllTickets = async (req, res) => {
   let data = await Ticket.find({});
@@ -113,4 +114,11 @@ const ticketBool = expressAsyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { buyTicket, ticketBool, addTicket, getAllTickets, Orderid };
+module.exports = {
+  buyTicket,
+  ticketBool,
+  addTicket,
+  getAllTickets,
+  Orderid,
+  getUserTickets,
+};
--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {buyTicket, addTicket, ticketBool, getAllTickets, getUserTickets} = require("../controllers/ticketControllers")
-
-router.route("/buy").post(buyTicket)
-router.route("/attendence/").post(ticketBool)
-router.route("/addTicket").post(addTicket)
-router.route("/getAllTs").get(getAllTickets)
-router.route("/getUserTickets").post(getUserTickets)
 const {
   buyTicket,
   addTicket,
   ticketBool,
   getAllTickets,
+  getUserTickets,
   Orderid,
 } = require("../controllers/ticketControllers");
 
@@ -19,6 +13,7 @@ router.route("/buy/:ticketId/:userId").post(buyTicket);
 router.route("/attendence/").post(ticketBool);
 router.route("/addTicket").post(addTicket);
 router.route("/getAllTs").get(getAllTickets);
-router.route("/getOrderId").get(Orderid);
+router.route("/getUserTickets").post(getUserTickets);
+router.route("/getOrderId/:ticketId").get(Orderid);
 
 module.exports = router;
